9c0a2254e8cbeb052aa6e018902e3554c8880f85
yaser <yaserbshaban@gmail.com>
2017-05-08 05:14:16 +0200

    @yasershaban  , Add two functions

--- llParsing/src/llparsing/GrammarManager.java
+++ llParsing/src/llparsing/GrammarManager.java
@@ -35,7 +35,17 @@ public void grammar() throws FileNotFoundException, IOException {
       } // end finally
 
 
-
+ // a function that return Strings  from sentance split by space  
+    public static String[] split(String word) {
+        String[] array = word.split("\\s+");
+        return array;
+    } // End of funcrion 
+ // a function that return first word in strings 
+    public static String returnFirst(String input) {
+        int i = input.indexOf(' ');
+        String word = input.substring(0, i);
+        return word;
+    } // End of funcrion
 
 } // end grammar()
 
