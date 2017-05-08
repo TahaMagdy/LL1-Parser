4f40e76485a01936beb41ce37d81a284f16934f2
TahaMagdy <t.tahamagdyt@outlook.com>
2017-05-08 05:10:44 +0200

    demo

--- llParsing/src/llparsing/GrammarManager.java
+++ llParsing/src/llparsing/GrammarManager.java
@@ -5,10 +5,45 @@
  */
 package llparsing;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 /**
  *
  * @author taha
  */
 public class GrammarManager {
 	
-}
+
+
+public void grammar() throws FileNotFoundException, IOException {
+
+	FileInputStream file = null;
+
+      try {
+         file = new FileInputStream("Grammer/Grammer.txt");
+         
+         int c;
+         while ((c = file.read()) != -1) {
+		System.out.print(c);
+         }
+      }finally {
+         if (file != null) {
+            file.close();
+         }
+      } // end finally
+
+
+
+
+} // end grammar()
+
+
+
+
+
+
+
+
+} // end GrammarManager
--- llParsing/src/llparsing/LlParsing.java
+++ llParsing/src/llparsing/LlParsing.java
@@ -15,7 +15,22 @@ public class LlParsing {
 	 * @param args the command line arguments
 	 */
 	public static void main(String[] args) {
-		// TODO code application logic here
-	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	} // end main
 	
 }
